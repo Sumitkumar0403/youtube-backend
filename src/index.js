5fa0413dab9d4cf66dfaefe34adb166407a55772
sumit-0403 <sumit16567@gmail.com>
2026-04-02 22:33:13 +0530

    added utilities

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-dotenv.config({path:"./.env"});
+dotenv.config({ path: "./.env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error("Error:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .then((err) => {
+    console.log("MongoDb Connection failed !!!", err);
+  });
 
 /*import express from "express";
 
