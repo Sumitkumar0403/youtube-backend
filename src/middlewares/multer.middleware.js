9a7bf13dec4382251c88a94bfb27b0a99e639181
sumit-0403 <sumit16567@gmail.com>
2026-04-05 23:03:07 +0530

    added image upload functionality

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../public/temp/"); // Specify the directory where you want to save the uploaded files
+  },
+  filename: function (req, file, cb) {
+    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    // cb(null, file.fieldname + "-" + uniqueSuffix);
+    cb(null, file.originalname); // Use the original file name
+  },
+});
+
+export const upload = multer({ storage });
