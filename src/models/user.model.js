2394d557c8cc96326371916522830b742581d128
sumit-0403 <sumit16567@gmail.com>
2026-04-13 18:55:16 +0530

    Added register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -30,7 +30,7 @@ const userSchema = new Schema(
     },
     watchhistory: [
       {
-        type: mongoose.Schema.Types.objectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: "Video",
       },
     ],
@@ -57,7 +57,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-userSchema.methods.generateAccessToken =  function () {
+userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
       _id: this._id,
